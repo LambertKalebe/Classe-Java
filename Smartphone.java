5366517e2429768092a76e910a561989c70c167f
Kalebe <kaka.leebe@gmail.com>
2024-05-26 17:25:03 -0300

    ok!

--- Smartphone.java
+++ Smartphone.java
@@ -4,16 +4,16 @@ public class Smartphone {
  * Classe que representa um celular e suas informações mais básicas. Essa classe foi feita para a resolução do exercício da EBAC no curso Desenvolvedor Full Stack Java v2, modulo 7
 */
 
-        String DeviceName = "Samsung Galaxy S23";
-        String ChipsetName = "Snapdragon 8 Gen 2";
-        String GPU = "Adreno 740";
-        int RAM = 8;
-        int Storage = 256;
+        String deviceName = "Samsung Galaxy S23";
+        String chipsetName = "Snapdragon 8 Gen 2";
+        String gpu = "Adreno 740";
+        int ram = 8;
+        int storage = 256;
 
-        System.out.println(DeviceName);
-        System.out.println(ChipsetName);
-        System.out.println(GPU);
-        System.out.println(RAM);
-        System.out.println(Storage);
+        System.out.println(deviceName);
+        System.out.println(chipsetName);
+        System.out.println(gpu);
+        System.out.println(ram);
+        System.out.println(storage);
     }
 }
