0b5fc7a54c21c4272bb0500ff26b2249c73ba9c6
Kalebe <kaka.leebe@gmail.com>
2024-05-26 17:21:40 -0300

    first commit

---
+++ Smartphone.java
@@ -0,0 +1,19 @@
+public class Smartphone {
+    public static void main(String[] args){
+/*
+ * Classe que representa um celular e suas informações mais básicas. Essa classe foi feita para a resolução do exercício da EBAC no curso Desenvolvedor Full Stack Java v2, modulo 7
+*/
+
+        String DeviceName = "Samsung Galaxy S23";
+        String ChipsetName = "Snapdragon 8 Gen 2";
+        String GPU = "Adreno 740";
+        int RAM = 8;
+        int Storage = 256;
+
+        System.out.println(DeviceName);
+        System.out.println(ChipsetName);
+        System.out.println(GPU);
+        System.out.println(RAM);
+        System.out.println(Storage);
+    }
+}
