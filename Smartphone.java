df73b18d6d4355a5a683a88ba86ecf75d049de7d
Kalebe <162381771+LambertKalebe@users.noreply.github.com>
2024-06-16 10:55:15 -0300

    Update Smartphone.java

--- Smartphone.java
+++ Smartphone.java
@@ -1,19 +1,54 @@
 public class Smartphone {
-    public static void main(String[] args){
-/*
- * Classe que representa um celular e suas informações mais básicas. Essa classe foi feita para a resolução do exercício da EBAC no curso Desenvolvedor Full Stack Java v2, modulo 7
-*/
+    // Propriedades
+    private String marca;
+    private String modelo;
+    private int capacidadeBateria;
+    private double tamanhoTela;
 
-        String deviceName = "Samsung Galaxy S23";
-        String chipsetName = "Snapdragon 8 Gen 2";
-        String gpu = "Adreno 740";
-        int ram = 8;
-        int storage = 256;
+    public Smartphone(String marca, String modelo, int capacidadeBateria, double tamanhoTela) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.capacidadeBateria = capacidadeBateria;
+        this.tamanhoTela = tamanhoTela;
+    }
 
-        System.out.println(deviceName);
-        System.out.println(chipsetName);
-        System.out.println(gpu);
-        System.out.println(ram);
-        System.out.println(storage);
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeBateria() {
+        return capacidadeBateria;
+    }
+
+    public void setCapacidadeBateria(int capacidadeBateria) {
+        this.capacidadeBateria = capacidadeBateria;
+    }
+
+    public double getTamanhoTela() {
+        return tamanhoTela;
+    }
+
+    public void setTamanhoTela(double tamanhoTela) {
+        this.tamanhoTela = tamanhoTela;
+    }
+
+    // Exibir informações do smartphone
+    public void exibirInformacoes() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Capacidade da Bateria: " + capacidadeBateria);
+        System.out.println("Tamanho da Tela: " + tamanhoTela);
     }
 }
